2bf38b406c2cc97f63d48da7877b5c4cd7f34b7f
RakibulHasan2 <rakibulhasan99445@gmail.com>
2022-11-08 17:18:57 +0600

    environment variable done

--- src/Components/SharedPage/Header/Header.js
+++ src/Components/SharedPage/Header/Header.js
@@ -13,9 +13,14 @@ const Header = () => {
          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
        </label>
        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-       <li><Link to='/'>Home</Link></li>
-        <li><a>Services</a></li>
-        <li><a>My Reviews</a></li>
+        <li><Link to='/'>Home</Link></li>
+        {
+          user?.uid?
+          <>
+          <li><a>Add Services</a></li>
+          <li><a>My Reviews</a></li>
+          </>: <p></p>
+        }
         <li><Link to='/blog'>Blogs</Link></li>
       </ul>
     </div>
@@ -24,9 +29,14 @@ const Header = () => {
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal p-0 font-bold">
-    <li><Link to='/'>Home</Link></li>
-    <li><a>Services</a></li>
-        <li><a>My Reviews</a></li>
+        <li><Link to='/'>Home</Link></li>
+        {
+          user?.uid?
+          <>
+          <li><a>Add Services</a></li>
+          <li><a>My Reviews</a></li>
+          </>: <p></p>
+        }
         <li><Link to='/blog'>Blogs</Link></li>
     </ul>
   </div>
--- src/firebaseConfig/firebaseConfig.js
+++ src/firebaseConfig/firebaseConfig.js
@@ -7,13 +7,19 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDpWKjHOh8cvkhiIq3HR0RCjF73FwG-teo",
-  authDomain: "true-care-e2eb9.firebaseapp.com",
-  projectId: "true-care-e2eb9",
-  storageBucket: "true-care-e2eb9.appspot.com",
-  messagingSenderId: "746438250734",
-  appId: "1:746438250734:web:7cc8628fce459c1e81a3ff",
-  measurementId: "G-MJG6KG8FT0"
+  // apiKey: "AIzaSyDpWKjHOh8cvkhiIq3HR0RCjF73FwG-teo",
+  // authDomain: "true-care-e2eb9.firebaseapp.com",
+  // projectId: "true-care-e2eb9",
+  // storageBucket: "true-care-e2eb9.appspot.com",
+  // messagingSenderId: "746438250734",
+  // appId: "1:746438250734:web:7cc8628fce459c1e81a3ff",
+  // measurementId: "G-MJG6KG8FT0"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
