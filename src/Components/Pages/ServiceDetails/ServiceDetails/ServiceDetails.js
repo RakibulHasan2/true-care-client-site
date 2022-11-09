091b16ef715834ed39d6750a2cd3ca05353648d1
RakibulHasan2 <rakibulhasan99445@gmail.com>
2022-11-09 15:55:46 +0600

    review done

--- src/Components/Pages/ServiceDetails/ServiceDetails/ServiceDetails.js
+++ src/Components/Pages/ServiceDetails/ServiceDetails/ServiceDetails.js
@@ -1,28 +1,47 @@
 import React, { useContext } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, Link } from 'react-router-dom';
 import { AuthContext } from '../../../../context/UserContext';
 import './ServiceDetails.css'
 const ServiceDetails = () => {
-    const {name, img, price, details, ratings}  = useLoaderData()
+    const {_id,name, img, price, details, ratings}  = useLoaderData()
     const {user} = useContext(AuthContext)
 
     const handlePlaceReview = event => {
         event.preventDefault();
         const form = event.target;
-        const name = form.Name.value;
+        const UserName = form.Name.value;
         const email = user?.email || 'unregistered';
-        const phone = form.phone.value;
         const message = form.message.value;
+        const photoURL = form.url.value
 
-        const order = {
-            // service: _id,
-            // serviceName: title,
+        const review = {
+            service: _id,
+            serviceName: name,
             price,
-            customer: name,
+            customer: UserName,
             email,
-            phone,
-            message
+            message,
+            photoURL
         }
+        fetch('http://localhost:5000/reviews', {
+            method: 'POST',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(review)
+        })
+        .then(res => res.json())
+        .then(data => { 
+            console.log(data)
+            if(data.acknowledged)
+            {
+                alert('review placed successfully')
+                form.reset()
+            }
+        })
+        .catch(err => console.log(err));
+
+
     }
     return (
             <div >
@@ -37,7 +56,8 @@ const ServiceDetails = () => {
            </div>
            <div className='review-section'>
            <form onSubmit={handlePlaceReview}>
-                <p className='text-3xl font-bold text-center mb-5'>Please Give Your Valuable Review</p>
+                <p className='text-3xl font-bold text-center mb-5'>Please Give Your Valuable Review in <span className='text-primary'>{name}</span></p>
+
                 <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mb-5'>
                     <input name="Name" type="text" placeholder="Your Name" className="input input-ghost w-full  input-bordered" />
                     <input name="email" type="text" placeholder="Your email" defaultValue={user?.email} className="input input-ghost w-full  input-bordered" readOnly />
@@ -45,7 +65,14 @@ const ServiceDetails = () => {
                 </div>
                 <textarea name="message" className="textarea textarea-bordered h-24 w-full mb-3" placeholder="Your Message" required></textarea>
 
-                <input className='btn mb-5' type="submit" value="Submit Review" />
+               {
+                user?.uid?
+                <>
+                 <input className='btn mb-5' type="submit" value="Submit Review" />
+                 </>
+                 :
+                 <> <Link to='/login'><input className='btn mb-5' type="submit" value="Submit Review" /></Link></>
+               }
             </form>
 
            </div>
